ee4d4af05c49cb2fe3712192379155de4ea2b903
Kevin Chen <kchen@kchen-01699-mac.local>
2021-07-12 17:55:12 -0700

    Add function to create sweep graph

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const model_sid = 'CDnv6RJDQ3d';
 const showcase = document.getElementById('showcase');
 const point_pos = document.getElementById('point_pos');
 const sweep_pos = document.getElementById('sweep_pos');
@@ -11,7 +10,40 @@ function pointToString(point) {
     return `(${x}, ${y}, ${z})`;
 }
 
-// check if domain is `kevinddchen.github.io` or `localhost`
+function distance(p1, p2) {
+    // Euclidean distance between two positions
+    return Math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2);
+}
+
+function createGraph(sweeps) {
+    // Given list of sweeps, return graph adjacency list.
+    // Returns: adjList[sweep_a_uuid][sweep_b_uuid] -> distance as float
+    const adjList = {};
+    const positions = {}; // keep track of sweep positions
+    for (let i=0; i<sweeps.length; i++) {
+        const swl_a = sweeps[i];
+        adjList[swl_a.uuid] = {};
+        positions[swl_a.uuid] = swl_a.position;
+
+        const neighbors = swl_a.neighbors;
+        for (let j=0; j<neighbors.length; j++) {
+            const swl_b_uuid = neighbors[j];
+            if (swl_b_uuid in adjList) { // if sweep already visited, we know its position
+                const d = distance(swl_a.position, positions[swl_b_uuid]);
+                adjList[swl_a.uuid][swl_b_uuid] = d;
+                adjList[swl_b_uuid][swl_a.uuid] = d;
+            }
+        }
+    }
+    return adjList;
+}
+
+// ----------------------------------------------------------------------------
+
+// Put model SID here
+const model_sid = 'CDnv6RJDQ3d';
+
+// Check if domain is `kevinddchen.github.io` or `localhost`, and pick SDK key accordingly
 let key;
 const domain = document.location.hostname
 if (domain == 'localhost') {
@@ -21,29 +53,38 @@ if (domain == 'localhost') {
 } else {
     console.log('Invalid domain name: '+domain)
 }
-showcase.src='bundle/showcase.html?m=CDnv6RJDQ3d&play=1&qs=1&applicationKey='+key;
-
-
+showcase.src='bundle/showcase.html?m='+model_sid+'&play=1&qs=1&applicationKey='+key;
 
 // --- START OF EMBEDDING ---
-
 showcase.addEventListener('load', async function() {
+
+    // connect to SDK
     let sdk;
     try {
         sdk = await showcase.contentWindow.MP_SDK.connect(showcase, key);
+        console.log('%cSDK Connected!', 'background: #333333; color: #00dd00');
     }
     catch(e) {
         console.error(e);
         return;
     }
 
-    console.log('%cSDK Connected!', 'background: #333333; color: #00dd00');
-    console.log(sdk);
-
+    // create node graph
+    let sweeps;
+    let adjList;
+    sdk.Model.getData().then( function(data) {
+        sweeps = data.sweeps;
+        adjList = createGraph(data.sweeps);
+        console.log(adjList);
+    });
+    
+    // track current sweep position
     sdk.Sweep.current.subscribe(function(currSweep) {
         sweep_pos.innerHTML = `current position: ${pointToString(currSweep.position)}`;
+        console.log(currSweep.uuid);
     });
 
+    // track pointer position
     sdk.Pointer.intersection.subscribe(function(interData) {
         point_pos.innerHTML = `pointer position: ${pointToString(interData.position)}`;
     });
